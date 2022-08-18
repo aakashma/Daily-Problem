8fd7955776026502cdd38969aeb1834e1a5d6f00
Akash Maurya <17akashma@gmail.com>
2022-08-18 07:27:10 +0530

    Update 31. Leetcode-Find intersection point of Y LinkedList.cpp

--- Striver's SDE Sheet/31. Leetcode-Find intersection point of Y LinkedList.cpp
+++ Striver's SDE Sheet/31. Leetcode-Find intersection point of Y LinkedList.cpp
@@ -1,2 +1,25 @@
 //leetcode-160. Intersection of Two Linked Lists
 //https://leetcode.com/problems/intersection-of-two-linked-lists/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_map<ListNode*,int>m;
+        while(headA!=NULL){
+            m[headA]++;
+            headA=headA->next;
+        }
+        while(headB!=NULL){
+            if(m[headB]>0) return headB;
+            headB=headB->next;
+        }
+        return NULL;
+    }
+};
